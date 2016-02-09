3690993187a28b2a56856e4328c3b182b7dc06d9
Tannin <sherb@gmx.net>
2016-02-09 19:38:24 +0100

    renamed project (and thus the output filename)

--- src/gamefallout3.h
+++ src/gamefallout3.h
@@ -6,9 +6,7 @@
 class GameFallout3 : public GameGamebryo
 {
   Q_OBJECT
-#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
   Q_PLUGIN_METADATA(IID "org.tannin.GameFallout3" FILE "gamefallout3.json")
-#endif
 
 public:
 
