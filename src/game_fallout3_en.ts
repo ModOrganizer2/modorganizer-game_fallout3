31a98283e6ff68a559aad34da151b563b61075a6
Tannin <sherb@gmx.net>
2016-06-04 20:31:47 +0200

    translation

---
+++ src/game_fallout3_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>GameFallout3</name>
+    <message>
+        <location filename="gamefallout3.cpp" line="74"/>
+        <source>Adds support for the game Fallout 3s</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
