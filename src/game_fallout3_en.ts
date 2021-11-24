f5a56a60a8372656c33ca60440c2a6a700c515a0
Chris Bessent <lost.dragonist@gmail.com>
2021-11-23 20:53:03 -0700

    Update translations

--- src/game_fallout3_en.ts
+++ src/game_fallout3_en.ts
@@ -4,13 +4,13 @@
 <context>
     <name>GameFallout3</name>
     <message>
-        <location filename="gamefallout3.cpp" line="81"/>
+        <location filename="gamefallout3.cpp" line="87"/>
         <source>Fallout 3 Support Plugin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gamefallout3.cpp" line="91"/>
-        <source>Adds support for the game Fallout 3s</source>
+        <location filename="gamefallout3.cpp" line="97"/>
+        <source>Adds support for the game Fallout 3.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
